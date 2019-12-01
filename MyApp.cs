e47d2cbc93652c0c86af128c4768a89e642e31fc
ben@web.de <ben@web.de>
2019-12-01 11:27:56 +0100

    Minor Change to master [without dev]

--- MyApp.cs
+++ MyApp.cs
@@ -5,7 +5,7 @@ using System.Windows; // the root WPF namespace
 // smallest programm possible
 namespace test{
  class MyApp {
- 
+ private String fetchtest;  
  // the WPF message box
  //MessageBox.Show("Hello, User, How do you do?");
  }
